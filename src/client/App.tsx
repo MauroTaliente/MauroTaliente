66b18c8a315dae510fc3541e83f86e1d01f5f98a
Mauro Taliente <maurotaliente@quiena.com>
2023-01-31 13:15:44 -0300

    fix import error

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import Main from './pages/Main';
 
 import { StyleGuideProvider } from './styles';
-import NoSSR from './components/build/wrap/NoSSR'; // TODO IMPOT FROM STYLEGIDE
 
 export const App = () => {
   return (
