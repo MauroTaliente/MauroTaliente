91e9b244dd363a8725a7d0662aadb10e04924ba2
Mauro Taliente <maurotaliente@quiena.com>
2023-02-07 19:04:05 -0300

    custom domine

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ cd dist/static/
 echo > .nojekyll
 
 # if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+echo 'www.maurotaliente.com' > CNAME
 
 git init
 git checkout -B main
